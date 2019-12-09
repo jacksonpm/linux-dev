27c21a72b3f6089a5fed1420de47508d5f36c0e3
jacksonpm <31311370+jacksonpm@users.noreply.github.com>
2019-12-09 09:05:52 -0300

    Create visual.sh

---
+++ visual.sh
@@ -0,0 +1,7 @@
+@echo Instalando Plank
+
+sudo add-apt-repository ppa:ricotz/docky -y && sudo apt-get update && sudo apt-get install plank -y
+sudo add-apt-repository ppa:docky-core/stable
+sudo apt-get update
+sudo apt-get install plank
+
