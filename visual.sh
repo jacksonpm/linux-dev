5fd0bd25532a51e9ccc71b6485ca7e566c9be13b
jacksonpm <31311370+jacksonpm@users.noreply.github.com>
2019-12-09 20:06:43 -0300

    Update visual.sh

--- visual.sh
+++ visual.sh
@@ -1,7 +1,11 @@
-@echo Instalando Plank
+echo Instalando Plank
 
 sudo add-apt-repository ppa:ricotz/docky -y && sudo apt-get update && sudo apt-get install plank -y
 sudo add-apt-repository ppa:docky-core/stable
 sudo apt-get update
 sudo apt-get install plank
 
+clear
+sudo apt install gnome-tweaks
+sudo apt remove gnome-shell-extension-ubuntu-dock
+sudo apt install vanilla-gnome-desktop
